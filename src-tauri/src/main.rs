58c747097a542572bfc9964995a08b09f0d6ca4a
lijia <lijia0732@sina.com>
2023-01-03 19:13:01 +0800

    feat: 内嵌文件, 并分别通过js、rust读取

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,22 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn read_conf(handle: tauri::AppHandle) -> String {
+    let resource_path = handle
+        .path_resolver()
+        .resolve_resource("resources/config/config.default.json")
+        .expect("failed to resolve resource");
+
+    let file = std::fs::File::open(&resource_path).unwrap();
+    let foo: serde_json::Value = serde_json::from_reader(file).unwrap();
+
+    foo.get("foo").unwrap().to_string()
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, read_conf])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { resolveResource } from '@tauri-apps/api/path'
+// alternatively, use `window.__TAURI__.path.resolveResource`
+import { readTextFile } from '@tauri-apps/api/fs'
+// alternatively, use `window.__TAURI__.fs.readTextFile`
 
 const greetMsg = ref("");
 const name = ref("");
@@ -9,12 +13,34 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsg.value = await invoke("greet", { name: name.value });
 }
+
+async function readConf() {
+  // `config.default.json` is the value specified on `tauri.conf.json > tauri > bundle > resources`
+  const resourcePath = await resolveResource('resources/config/config.default.json');
+  const conf = JSON.parse(await readTextFile(resourcePath));
+  console.log(conf);
+  greetMsg.value = "read using js: " + conf.foo;
+}
+
+async function readConfFromRust() {
+  greetMsg.value = "read using rust: " + await invoke("read_conf");
+}
+
 </script>
 
+<style>
+.btn {
+  margin-top: 5px;
+}
+</style>
 <template>
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="button" @click="greet()">Greet</button>
+  </div>
+  <div class="btn" >
+    <button type="button" @click="readConf()">ReadConf</button>
+    <button type="button" @click="readConfFromRust()">ReadConf2</button>
   </div>
 
   <p>{{ greetMsg }}</p>
