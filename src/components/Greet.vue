c1c6805c0373c3b0f6d09942f2b1d5d4befefe1d
lijia <lijia0732@sina.com>
2023-01-03 21:36:22 +0800

    feat: 自更新

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -6,6 +6,10 @@ import { resolveResource } from '@tauri-apps/api/path'
 import { readTextFile } from '@tauri-apps/api/fs'
 // alternatively, use `window.__TAURI__.fs.readTextFile`
 
+import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
+
+
 const greetMsg = ref("");
 const name = ref("");
 
@@ -26,6 +30,20 @@ async function readConfFromRust() {
   greetMsg.value = "read using rust: " + await invoke("read_conf");
 }
 
+async function update() {
+  try {
+    const { shouldUpdate, manifest } = await checkUpdate()
+    console.log(shouldUpdate, manifest);
+    if (shouldUpdate) {
+      // display dialog
+      await installUpdate()
+      // install complete, restart the app
+      await relaunch()
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <style>
@@ -38,9 +56,10 @@ async function readConfFromRust() {
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="button" @click="greet()">Greet</button>
   </div>
-  <div class="btn" >
+  <div class="btn">
     <button type="button" @click="readConf()">ReadConf</button>
     <button type="button" @click="readConfFromRust()">ReadConf2</button>
+    <button type="button" @click="update()">检查更新</button>
   </div>
 
   <p>{{ greetMsg }}</p>
