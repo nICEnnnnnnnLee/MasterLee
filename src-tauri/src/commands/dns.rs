8f4ce3427f691d32122d9b1edeccdbd7b1b26350
lijia <lijia0732@sina.com>
2023-01-09 13:30:11 +0800

    feat: 增加相关配置的保存与读取

--- src-tauri/src/commands/dns.rs
+++ src-tauri/src/commands/dns.rs
@@ -1,21 +1,64 @@
+use std::{time::Duration, vec};
 
-use std::time::Duration;
-
-use crate::{
-    core::{dns::{self, TestDnsMethod}, hosts},
+use crate::core::{
+    dns::{self, TestDnsMethod, DoHServerConfig},
+    hosts,
 };
 use tokio::try_join;
+
+#[tauri::command]
+pub fn add_doh_servers(conf: Vec<&str>) {
+    for record in conf.iter() {
+        let server = DoHServerConfig::new(record.to_string());
+        dns::DOH_SERVERS.insert(server.domain.clone(), server);
+    }
+    dns::init_doh_resolvers();
+    println!("以下是可用的doh：");
+    for entry in dns::DOH_SERVERS.iter() {
+        println!("{}", entry.key());
+    }
+}
+
+#[tauri::command]
+pub fn add_hosts(conf: Vec<&str>) {
+    for record in conf.iter() {
+        let v: Vec<&str> = record.split(" ").collect();
+        hosts::put(v[v.len() - 1].to_string(), v[0].to_string());
+    }
+    println!("以下是host：");
+    for entry in hosts::HOSTS.iter() {
+        println!("{} -> {}", entry.key(), entry.value());
+    }
+}
+
+#[tauri::command]
+pub fn get_all_hosts() -> Vec<String> {
+    let mut result = vec![];
+    for entry in hosts::HOSTS.iter() {
+        result.push(format!("{} {}", entry.value(), entry.key()));
+    }
+    result
+}
+
+#[tauri::command]
+pub fn get_all_doh_servers() -> Vec<String> {
+    let mut result = vec![];
+    for entry in dns::DOH_SERVERS.iter() {
+        result.push(format!("{}", entry.key()));
+    }
+    result
+}
 
 #[tauri::command]
 pub async fn pull_doh_servers_and_set_resolvers() {
     // set doh server
     dns::collect_valid_doh_servers(Duration::from_secs(2))
-                .await
-                .unwrap();
+        .await
+        .unwrap();
     // init resolvers
     dns::init_doh_resolvers();
     println!("以下是可用的doh：");
-    for entry in dns::DOH_SERVERS.iter(){
+    for entry in dns::DOH_SERVERS.iter() {
         println!("{}", entry.key());
     }
 }
@@ -24,8 +67,8 @@ pub async fn pull_doh_servers_and_set_resolvers() {
 pub async fn query_dns_and_set_host(domains: Vec<String>) {
     // query for domain
     let mut handles = vec![];
-    for domain in domains{
-        let handle = tokio::spawn(async move{
+    for domain in domains {
+        let handle = tokio::spawn(async move {
             // println!("查询{} 开始...", domain);
             let clone = domain.clone();
             let ips = dns::query_domain_for_valid_ips(
@@ -33,11 +76,13 @@ pub async fn query_dns_and_set_host(domains: Vec<String>) {
                 TestDnsMethod::HTTPS,
                 false,
                 Duration::from_secs(2),
-            ).await.unwrap();
-            if ips.len() > 0{
+            )
+            .await
+            .unwrap();
+            if ips.len() > 0 {
                 let mut best_result = &ips[0];
-                for result in ips.iter(){
-                    if result.cost < best_result.cost{
+                for result in ips.iter() {
+                    if result.cost < best_result.cost {
                         best_result = result;
                     }
                 }
@@ -47,12 +92,11 @@ pub async fn query_dns_and_set_host(domains: Vec<String>) {
         });
         handles.push(handle);
     }
-    for handle in handles{
+    for handle in handles {
         let _ = try_join!(handle);
     }
     println!("以下是host：");
-    for entry in hosts::HOSTS.iter(){
+    for entry in hosts::HOSTS.iter() {
         println!("{} -> {}", entry.key(), entry.value());
     }
 }
-
--- src/components/main/DoH.vue
+++ src/components/main/DoH.vue
@@ -1,15 +1,58 @@
 <script setup>
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { resolveResource } from '@tauri-apps/api/path'
+import { readTextFile, writeTextFile, createDir } from '@tauri-apps/api/fs'
 
-const tips_pull_doh_servers_and_set_resolvers = ref("当前尚未设置doh");
+const tips = ref("当前尚未设置doh");
+const init = () => {
+  return new Promise(function (resolve, reject) {
+    readConf().then((conf) => {
+      invoke('add_doh_servers', { conf })
+        .then(() => {
+          tips.value = "已加载配置"
+          resolve()
+        })
+        .catch((e) => reject(e))
+    }).catch((e) => reject(e))
+  })
+
+}
+
+defineExpose({
+  init
+})
+
+async function readConf() {
+  const dir = await resolveResource('data')
+  const path = await resolveResource('data/doh.txt')
+  await createDir(dir, { recursive: true })
+  const conf = await readTextFile(path);
+  return conf.split("\n").map(val => val.trim()).filter(val => val != "" && !val.startsWith("#"))
+}
+
+async function saveConf(conf) {
+  const dir = await resolveResource('data')
+  const path = await resolveResource('data/doh.txt')
+  await createDir(dir, { recursive: true })
+  let sortedConf = conf.sort((a, b) => a > b ? 1 : a == b ? 0 : -1)
+  await writeTextFile(path, sortedConf.join("\n"))
+}
+
 async function pull_doh_servers_and_set_resolvers() {
-  tips_pull_doh_servers_and_set_resolvers.value = "进行中..."
+  tips.value = "进行中..."
   invoke("pull_doh_servers_and_set_resolvers").then(() => {
-    tips_pull_doh_servers_and_set_resolvers.value = "已完成..."
+    tips.value = "已完成..."
+    invoke('get_all_doh_servers')
+      .then((dohs) => {
+        console.log(dohs)
+        saveConf(dohs).await
+      })
+      .catch((e) => console.error(e))
+
   }).catch((err) => {
     console.log(err);
-    tips_pull_doh_servers_and_set_resolvers.value = "出现异常"
+    tips.value = "出现异常"
   })
 }
 </script>
@@ -19,7 +62,7 @@ async function pull_doh_servers_and_set_resolvers() {
   <div class="row">
     <div class="btn">
       <button type="button" @click="pull_doh_servers_and_set_resolvers()">查找可用的DoH</button>
-      <span class="tips">{{ tips_pull_doh_servers_and_set_resolvers }}</span>
+      <span class="tips">{{ tips }}</span>
     </div>
   </div>
 </template>
@@ -45,5 +88,4 @@ button {
   width: 190px;
   max-height: 40px;
 }
-
 </style>
