6eceb7f2769d7a5e6b8dd1ca091b6d0ff2267f9d
lijia <lijia0732@sina.com>
2023-01-12 15:31:32 +0800

    feat: 打开代理时增加成功与否的判断

--- src-tauri/src/commands/proxy.rs
+++ src-tauri/src/commands/proxy.rs
@@ -1,13 +1,10 @@
 use std::net::ToSocketAddrs;
 
-use crate::{
-    core::{http_proxy},
-    state,
-};
+use crate::{core::http_proxy, state};
 use tauri::Manager;
 use tokio::{io, net::TcpListener};
 
-async fn run(addr: &str, port: u16) -> io::Result<()> {
+async fn bind_server(addr: &str, port: u16) -> io::Result<TcpListener> {
     let addr = (addr, port)
         .to_socket_addrs()?
         .next()
@@ -16,6 +13,10 @@ async fn run(addr: &str, port: u16) -> io::Result<()> {
     // 监听TCP连接
     let listener = TcpListener::bind(&addr).await?;
     http_proxy::set_bind_port(port);
+    Ok(listener)
+}
+
+async fn run(listener: TcpListener) -> io::Result<()> {
     loop {
         if let Ok((stream, _peer_addr)) = listener.accept().await {
             tokio::spawn(async move {
@@ -26,6 +27,25 @@ async fn run(addr: &str, port: u16) -> io::Result<()> {
         }
     }
 }
+// async fn run(addr: &str, port: u16) -> io::Result<()> {
+//     let addr = (addr, port)
+//         .to_socket_addrs()?
+//         .next()
+//         .ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
+
+//     // 监听TCP连接
+//     let listener = TcpListener::bind(&addr).await?;
+//     http_proxy::set_bind_port(port);
+//     loop {
+//         if let Ok((stream, _peer_addr)) = listener.accept().await {
+//             tokio::spawn(async move {
+//                 if let Err(_err) = http_proxy::handle(stream).await {
+//                     // eprintln!("{:?}", _err);
+//                 }
+//             });
+//         }
+//     }
+// }
 #[tauri::command]
 pub fn start_proxy2(_port: u32) {
     // tauri::async_runtime::spawn(async move {
@@ -53,22 +73,31 @@ pub async fn start_proxy(addr: String, port: u16, window: tauri::Window) {
             .build()
             .unwrap();
         let handle = runtime.handle().clone();
+        let window_clone = window.clone();
         window.once_global("stop-proxy", |_event| {
             println!("接收到 stop-proxy");
-            state::enable_proxy(false);
             runtime.shutdown_background();
+            state::enable_proxy(false);
         });
         let task_run = handle.spawn(async move {
-            run(&addr, port).await.unwrap();
-            println!("stopped");
+            if let Ok(listener) = bind_server(&addr, port).await {
+                window_clone.emit("proxy-start", true).unwrap();
+                state::enable_proxy(true);
+                run(listener).await.unwrap();
+                println!("stopped");
+            } else {
+                state::enable_proxy(false);
+                window_clone.emit("proxy-start", false).unwrap();
+            }
         });
 
         tauri::async_runtime::spawn(async move {
             let _ = task_run.await;
         });
-        state::enable_proxy(true);
+        
         println!("start_proxy end...");
     } else {
+        window.emit("proxy-start", true).unwrap();
         println!("proxy 已开启...");
     }
 }
